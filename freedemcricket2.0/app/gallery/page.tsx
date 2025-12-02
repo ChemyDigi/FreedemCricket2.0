bc8e2b3c4c396ae67620ca355c548f91e9c40476
nethasa-manawadu <dunithi.nethasa@gmail.com>
2025-12-02 13:53:05 +0530

    update page.tsx | gallery

--- freedemcricket2.0/app/gallery/page.tsx
+++ freedemcricket2.0/app/gallery/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/components/Navbar";
 import MasonryGallery from "@/components/Gallery/MansonryGallery";
+import Footer from "@/components/Footer";
 
 export default function GalleryPage() {
   return (
     <div className="min-h-screen">
+        <Navbar/>
         <MasonryGallery/>
+        <Footer/>
     </div>
   );
 }
