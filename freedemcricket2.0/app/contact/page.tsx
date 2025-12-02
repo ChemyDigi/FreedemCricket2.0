5f68abf9c8414749067263a9588268e917a3a4a4
aathif2004 <aathifnainarali2@gmail.com>
2025-12-02 12:23:40 +0530

    Contact us page all done

--- freedemcricket2.0/app/contact/page.tsx
+++ freedemcricket2.0/app/contact/page.tsx
@@ -1,11 +1,23 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Top from '@/components/ContactUs/ContactSection';
+import Map from '@/components/ContactUs/MapSection';
+import Footer from '@/components/Footer';
+import HeroSection from '@/components/HeroSection';
+import Nav from '@/components/Navbar';
 
-export default function HomePage() {
+export default function ContactPage() {
     return (
-        <>
-            <Navbar />
+        <div className="flex flex-col">
+            <Nav />
+            <HeroSection
+            backgroundImage="/images/ContactUs/contactHero.png"
+            title="Contact "
+            highlight="Us"
+            subtitle="-Your Questions, Our Commitment to the Game.-"
+            ribbonImage="/images/ContactUs/zigzagline.png"
+        />
+            <Top />
+            <Map />
             <Footer />
-        </>
+        </div>
     );
-}
+}   
