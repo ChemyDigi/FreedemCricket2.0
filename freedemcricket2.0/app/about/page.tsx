844fb49f4516578cda97936fb4783a723819ce20
Madukaaa <madukakarunathilake2003@gmail.com>
2025-12-02 12:00:18 +0530

    Added the trainers section

---
+++ freedemcricket2.0/app/about/page.tsx
@@ -0,0 +1,9 @@
+import Trainers from "@/components/AboutUs/Trainers";
+
+export default function AboutPage() {
+  return (
+    <section className="min-h-[60vh]">
+      <Trainers />
+    </section>
+  );
+}
