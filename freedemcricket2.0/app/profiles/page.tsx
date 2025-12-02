1a0252cd7540c48c4c8a7ffb2f9152a513253cf8
Naduni Peiris <peirisnaduni13@gmail.com>
2025-12-02 12:31:12 +0530

    coach profiles

---
+++ freedemcricket2.0/app/profiles/page.tsx
@@ -0,0 +1,10 @@
+import IntroCoach from "@/components/Profile/IntroCoach";
+import CoachSection from "@/components/Profile/CoachSection";
+export default function Profile() {
+  return (
+    <main className="min-h-screen bg-black">
+      <IntroCoach />
+      <CoachSection/>
+    </main>
+  );
+}
